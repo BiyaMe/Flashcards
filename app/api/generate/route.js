aec981d2b601dff586d054fa18ce102c63a93513
BiyaMe <biyamesay19@gmail.com>
2024-08-18 18:03:10 +0300

    last

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
+//const { GoogleGenerativeAI } = require("@google/generative-ai");
+// import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const systemPrompt = `
 You are a flashcard creator
@@ -44,3 +46,45 @@ export async function POST(req){
 
     return NextResponse.json(flashcards.flashcards)
 }
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+
+
+// const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+// const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+// export async function POST(req) {
+//     try {
+//         const data = await req.text();
+
+// const completion = await genAI.chat.completions.create({
+//     messages: [
+//         { role: 'system', content: systemPrompt },
+//         { role: 'user', content: data }
+//     ],
+//     model: 'gemini-1.5-flash',
+//     response_format: { type: 'json_object' }
+// });
+
+//         const completion = await genAI.flashcards.create({
+//             prompt: data,
+//             max_tokens: 100,
+//             temperature: 0.5,
+//             top_p: 1,
+//             frequency_penalty: 0,
+//             presence_penalty: 0,
+//             stop: ['\n']
+//         });
+
+
+//         if (completion && completion.choices && completion.choices[0].message) {
+//             const flashcards = JSON.parse(completion.choices[0].message.content);
+//             return NextResponse.json(flashcards.flashcards);
+//         } else {
+//             return NextResponse.error("Completion data is invalid or missing.");
+//         }
+//     } catch (error) {
+//         console.error("An error occurred:", error);
+//         return NextResponse.error("An error occurred. Please check the server logs for more details.");
+//     }
+// }
